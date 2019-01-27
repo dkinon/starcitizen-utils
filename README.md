72d3dd06a954f7a07166ef851a2e601bb579195a
Dan Kinon <dan.kinon@gmail.com>
2019-01-26 22:04:15 -0800

    Initial addition of sc-perf.ps1 Update docs

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # starcitizen-utils
 Star Citizen helper scripts and stuff
+
+
+## Prerequisites
+* A Windows installation with a [StarCitizen](https://robertsspaceindustries.com) installed.
+* [Git SCM](https://git-scm.com/download/win) or other git solution for Windows.
+* Windows PowerShell(https://docs.microsoft.com/en-us/powershell/scripting/install/installing-windows-powershell)
+
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned -Force
+
+## Quickstart
+1. Clone the repo to the directory of your choosing.
+2. Launch Windows PowerShell
+![GitHub Logo](./docs/screenshots/windows10-startmenu.png)
+3. 
+`
+PS >Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned -Force
+`
+4. Start sc-util.ps1
+`PS > ./sc-util.ps1`
+
+![Sample Output](./docs/screenshot/powershell-output.png)
---
+++ sc-perf.ps1
@@ -0,0 +1,92 @@
+<#
+  Star Citizen Performance Manager
+
+
+  Changelog:
+  * Sat Jan 25 2019 Dan Kinon <dan.kinon@gmail.com>
+  - Initial script with infinite loop and StarCitizen process priority monitoring
+#>
+
+Function Set-Priority { 
+  <#
+  .SYNOPSIS    
+   Set the process priority of the current Powershell session 
+  .PARAMETER priority 
+   The priority as an integer value from -2 to 3.  -2 is the lowest, 0 is the default (normal) and 3 is the highest (which may require Administrator privilege) 
+  .PARAMETER processID 
+   The process ID that will be change.  Omit to set the current powershell session. 
+  .PARAMETER silent 
+   Suppress the message at the end 
+  .EXAMPLE   
+   Set-PSPriority 2  
+  #>
+ 
+  param ( 
+    [ValidateRange(-2,3)]  
+    [Parameter(Mandatory=$true)] 
+    [int]$priority, 
+    [int]$processID = $pid, 
+    [switch]$silent 
+  )
+ 
+  $priorityhash = @{-2="Idle";-1="BelowNormal";0="Normal";1="AboveNormal";2="High";3="RealTime"}
+  (Get-Process -Id $processID).priorityclass = $priorityhash[$priority] 
+ 
+  if (!$silent) { 
+    "Process ID [$processID] is now set to " + (Get-Process -Id $pid).priorityclass 
+  }
+}
+
+
+$ScriptName = "Star Citizen Performance Manager"
+$ProcessName = "StarCitizen"
+$PriorityHash = @{-2="Idle";-1="BelowNormal";0="Normal";1="AboveNormal";2="High";3="RealTime"}
+$DesiredPriority = 2
+$PollingSeconds = 10
+
+<# System Specs #>
+$CpuCores = (Get-WmiObject Win32_Processor).NumberOfLogicalProcessors
+$PhysicalMemory = [math]::Round((Get-WmiObject Win32_ComputerSystem).TotalPhysicalMemory/1MB)
+$VirtualMemory = [math]::Round((Get-WmiObject Win32_ComputerSystem).TotalVirtualMemory/1MB)
+
+Write-Host "$(Get-Date -Format u): Welcome to $ScriptName"
+Write-Host "$(Get-Date -Format u): CPU Cores=$CpuCores, Physical Memory=$($PhysicalMemory)MB, Virtual Memory=$($VirtualMemory)MB"
+
+<# GPU Specs #>
+foreach ( $gpu in Get-WmiObject Win32_VideoController) {
+    $name = $gpu.Name
+    $driverVersion = $gpu.DriverVersion
+    $resolution = "$($gpu.CurrentHorizontalResolution)x$($gpu.CurrentVerticalResolution) @ $($gpu.MaxRefreshRate)Hz"
+    Write-Host "$(Get-Date -Format u): $name [$driverVersion] running at $resolution"
+}
+
+
+
+while( $true ) {
+  $ProcessActive = Get-Process $ProcessName -ErrorAction SilentlyContinue
+  if( $ProcessActive -eq $null ) {
+    Write-host "$(Get-Date -Format u): $ProcessName is not running"
+  } else {
+    $physicalMemory = [math]::Round($ProcessActive.WS/1MB)
+    $virtualMemory = [math]::Round($ProcessActive.VM/1MB)
+    Write-host "$(Get-Date -Format u): $ProcessName [$($ProcessActive.Id)] Priority=$($ProcessActive.priorityClass), Physical Memory=$physicalMemory, Virtual Memory=$virtualMemory"
+
+    
+    Get-WmiObject Win32_PerfRawData_PerfProc_Process |
+    Sort PercentProcessorTime -descending | Select -first 5 Name,
+    @{Name="PercentProcessorTime";Expression={($_.PercentProcessorTime/100000/100)/60}},
+    IDProcess | format-table -autosize
+
+
+    <# Set Process Priority #>
+    if( $ProcessActive.PriorityClass -ne $Priorityhash[$DesiredPriority] ) {
+      Write-host "$(Get-Date -Format u): setting process priority to $($Priorityhash[$DesiredPriority])"
+      $ProcessActive.PriorityClass = $Priorityhash[$DesiredPriority]
+    }
+  }
+
+  <# gps | where {$_.priorityclass -eq 'AboveNormal' -OR $_.priorityclass -eq 'High' -OR $_.priorityclass -eq 'RealTime'} | select name, priorityclass #>
+  <# $i++
+  Write-Host "$(Get-Date -Format u): We have counted up to $i" #>
+  sleep $PollingSeconds
+}
